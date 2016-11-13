ef145f2b96aad02f33b408af803aad4fd9e346c5
MakhSala <salahmakhloufi@gmail.com>
2016-11-13 00:13:15 -0500

    Prim application
    New prim method

---
+++ prim_application.py
@@ -0,0 +1,56 @@
+import numpy as np
+import random
+from node import Node
+from edge import Edge
+from graph import *
+from acm import *
+from ensemble_node import EnsembleNode
+
+
+graph = Graph(name='Application cours Prim')
+alphabet = list('ABCDEFGHI')
+for k in alphabet:
+    node_k = Node(name=k)
+    graph.add_node(node_k)
+
+edge = Edge(name='AB', startnode=graph.nodes[0], endnode=graph.nodes[1], cost=4.0)
+graph.add_edge(edge)
+edge = Edge(name='BC', startnode=graph.nodes[1], endnode=graph.nodes[2], cost=8.0)
+graph.add_edge(edge)
+edge = Edge(name='CD', startnode=graph.nodes[2], endnode=graph.nodes[3], cost=7.0)
+graph.add_edge(edge)
+edge = Edge(name='DE', startnode=graph.nodes[3], endnode=graph.nodes[4], cost=9.0)
+graph.add_edge(edge)
+edge = Edge(name='EF', startnode=graph.nodes[4], endnode=graph.nodes[5], cost=10.0)
+graph.add_edge(edge)
+edge = Edge(name='FG', startnode=graph.nodes[5], endnode=graph.nodes[6], cost=2.0)
+graph.add_edge(edge)
+edge = Edge(name='GH', startnode=graph.nodes[6], endnode=graph.nodes[7], cost=1.0)
+graph.add_edge(edge)
+edge = Edge(name='AH', startnode=graph.nodes[7], endnode=graph.nodes[0], cost=8.0)
+graph.add_edge(edge)
+edge = Edge(name='DF', startnode=graph.nodes[3], endnode=graph.nodes[5], cost=14.0)
+graph.add_edge(edge)
+edge = Edge(name='FC', startnode=graph.nodes[5], endnode=graph.nodes[2], cost=4.0)
+graph.add_edge(edge)
+edge = Edge(name='CI', startnode=graph.nodes[2], endnode=graph.nodes[8], cost=2.0)
+graph.add_edge(edge)
+edge = Edge(name='IH', startnode=graph.nodes[8], endnode=graph.nodes[7], cost=7.0)
+graph.add_edge(edge)
+edge = Edge(name='IG', startnode=graph.nodes[8], endnode=graph.nodes[6], cost=6.0)
+graph.add_edge(edge)
+edge = Edge(name='BH', startnode=graph.nodes[8], endnode=graph.nodes[7], cost=7.0)
+graph.add_edge(edge)
+
+#construct MWST of the example graph, plot it and print its weight
+prim_st = graph.prim2()
+#prim_st = graph.prim()
+
+
+
+print 'MST WEIGHT : ' + str(prim_st.get_graph_weight())
+for e in prim_st.edges:
+    print e.edge_name,
+    print e.edge_cost
+
+prim_st.plot_graph()
